c3175d964708e60c58eff9ec81ebd871315e4a48
Jan Rey Suarez <w33haa2@gmail.com>
2020-04-22 18:12:39 +0800

    added pgsql config

--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
 
+$DATABASE_URL=parse_url('postgres://pkjnkiklrczgip:95a2405501a935435ade2b41721df72d6b909e2593068e8d59a87a192b99a87d@ec2-54-75-229-28.eu-west-1.compute.amazonaws.com:5432/db4i2n1fttfqo');
+
 use Illuminate\Support\Str;
 
 return [
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +68,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' =>  $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,

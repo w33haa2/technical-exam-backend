649b3df11d8c18f8ad55d84bb5e67db4defa9891
Jan Rey Suarez <w33haa2@gmail.com>
2020-04-23 22:13:42 +0800

    added xd

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| API Routes xd
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
